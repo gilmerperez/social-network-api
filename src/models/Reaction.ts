c24890f2877624870e694c2ae3e8c32b620abd46
gilmerperez <gilmer2002@outlook.com>
2025-02-25 10:28:32 -0500

    formatted Reaction Schema

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -1,42 +1,40 @@
-import { Schema, model, type Document, Types } from 'mongoose';
+import { Schema, type Document, Types } from 'mongoose';
 
 // Reaction Interface (SCHEMA ONLY)
 interface IReaction extends Document {
-    reactionId: Schema.Types.ObjectId;
-    reactionBody: string;
-    username: string;
-    createdAt: Date;
+  reactionId: Schema.Types.ObjectId;
+  reactionBody: string;
+  username: string;
+  createdAt: Date;
 }
 
 // Reaction Schema
 const reactionSchema = new Schema<IReaction>(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: [280, 'Cannot exceed 280 characters']
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createdAt: {
-            type: Date,
-            default: () => new Date(),
-        },
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        timestamps: true
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: [280, 'Cannot exceed 280 characters']
     },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: () => new Date(),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    timestamps: true
+  },
 );
-
-// const Reaction = model<IReaction>('Reaction', reactionSchema);
 
 export default reactionSchema;
