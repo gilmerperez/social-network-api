bd61338b4c391b5080bc74deadddf50bf9b7ac3a
gilmerperez <gilmer2002@outlook.com>
2025-02-25 21:25:08 -0500

    Fixed seed.ts file name, added placeholder routes to users and thoughts for now

--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -3,5 +3,7 @@ const router = Router();
 
 // Import user controller functions
 
+router.get('/', (_req, res) => res.send('User route working!'));
+
 // Export the router to be used in `api/index.ts`
 export default router;
